04dfbe9e7c1b660476fe20c573ecd5cb4c8ecec3
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-02-02 19:15:19 -0800

    Initial commit

--- server.js
+++ server.js
@@ -8,4 +8,22 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
+io.on('connection', function (socket) {
+    console.log("io server is connected");
+
+    socket.on('draw', function (drawing) {
+        console.log("start drawing");
+
+        socket.broadcast.emit('beginDrawing', drawing);
+        console.log("server drawing function is working")
+    })
+})
+
+
+
+
+
+
+
 server.listen(process.env.PORT || 8080);
+console.log("server listening on port 8080");
