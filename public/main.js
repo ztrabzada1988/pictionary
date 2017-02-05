424d3309fa9ebaca93bb98269a6ae5a1e929b6a0
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-02-04 16:09:25 -0800

    completing client and server communications

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-
     var socket = io();
+    var drawing = false;
 
     var pictionary = function () {
         var canvas, context;
@@ -15,16 +15,31 @@ $(document).ready(function () {
         context = canvas[0].getContext('2d');
         canvas[0].width = canvas[0].offsetWidth;
         canvas[0].height = canvas[0].offsetHeight;
-        canvas.on('mousemove', function (event) {
-            var offset = canvas.offset();
-            var position = {
-                x: event.pageX - offset.left,
-                y: event.pageY - offset.top
-            };
 
-            draw(position);
-            // send to server
-            socket.emit('draw', position);
+        canvas.on('mousedown', function (event) {
+            drawing = true;
+            console.log("mousedown is activated");
+        });
+
+        canvas.on('mouseup', function (event) {
+            drawing = false;
+            console.log("mouseup is activated");
+        })
+
+
+        canvas.on('mouseover', function (event) {
+
+            if (drawing == true) {
+
+                var offset = canvas.offset();
+                var position = {
+                    x: event.pageX - offset.left,
+                    y: event.pageY - offset.top
+                };
+                draw(position);
+                // send to server
+                socket.emit('draw', position);
+            }
         });
 
         socket.on('beginDrawing', draw);
@@ -33,4 +48,4 @@ $(document).ready(function () {
 
     pictionary();
 
-})
+});
